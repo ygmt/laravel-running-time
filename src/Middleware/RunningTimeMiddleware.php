b374120515612f175a1bd686de4b9ead2ba88b80
wangh <704159579@qq.com>
2020-09-19 17:04:08 +0800

    add response

--- src/Middleware/RunningTimeMiddleware.php
+++ src/Middleware/RunningTimeMiddleware.php
@@ -7,139 +7,139 @@ use Closure;
 class RunningTimeMiddleware
 {
 
-    /**
-     * @var bool
-     */
-    protected $isDelayMode;
+	/**
+	 * @var bool
+	 */
+	protected $isDelayMode;
 
-    protected $redisList;
+	protected $redisList;
 
-    protected $redisTime;
+	protected $redisTime;
 
-    protected $redis;
+	protected $redis;
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $response = $next($request);
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Closure $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		$response = $next($request);
 
-        $path = $request->path();
-        if ($request->route()) {
-            $path = $request->route()->uri();
-        }
+		$path = $request->path();
+		if ($request->route()) {
+			$path = $request->route()->uri();
+		}
 
-        $route = $path;
-        $path = $request->method() . ':' . $path;
+		$route = $path;
+		$path = $request->method() . ':' . $path;
 
-        $this->isDelayMode = config('runningtime.mode') == 'delay';
-        $this->redisList = config('app.name') . ':laravel_running_time:list';
-        $this->redisTime = config('app.name') . ':laravel_running_time:time';
-        $this->isDelayMode && $this->redis = app('redis');
+		$this->isDelayMode = config('runningtime.mode') == 'delay';
+		$this->redisList = config('app.name') . ':laravel_running_time:list';
+		$this->redisTime = config('app.name') . ':laravel_running_time:time';
+		$this->isDelayMode && $this->redis = app('redis');
 
-	    
-        if (!app()->runningInConsole()) {
-            $log = [
-                'time' => round(microtime(true) - LARAVEL_START, 2),
-                'path' => $path,
-                'params' => json_encode([
-                		'route_params' => $request->route() ? $request->route()->parameters() : null,
-		                'params' => $request->all(),
 
-                ], JSON_UNESCAPED_UNICODE),
-	            'route' => $route,
-	            'method' => $request->method(),
-	            'header' => $request->header(),
-	            'client_ip' => $request->ip(),
-	            'created_at' => date('Y-m-d H:i:s'),
-            ];
+		if (!app()->runningInConsole()) {
+			$log = [
+				'time' => round(microtime(true) - LARAVEL_START, 5),
+				'path' => $path,
+				'params' => json_encode([
+					'route_params' => $request->route() ? $request->route()->parameters() : null,
+					'params' => $request->all(),
+				], JSON_UNESCAPED_UNICODE),
+				'route' => $route,
+				'method' => $request->method(),
+				'header' => json_encode([$request->header()], JSON_UNESCAPED_UNICODE),
+				'client_ip' => $request->ip(),
+				'created_at' => date('Y-m-d H:i:s'),
+				'response' => is_array($response) ? json_encode($response, JSON_UNESCAPED_UNICODE) : null,
+			];
 
-            $logText = implode('||', $log) . "\n";
+			$logText = implode('||', $log) . "\n";
 
-            $this->writeRequestLog($logText);
-        }
+			$this->writeRequestLog($logText);
+		}
 
-        return $response;
-    }
+		return $response;
+	}
 
-    /**
-     * Write request time to log.
-     *
-     * @param string $data
-     */
-    private function writeRequestLog($data)
-    {
-        if ($this->isDelayMode) {
-            $this->redis->rpush($this->redisList, $data);
+	/**
+	 * Write request time to log.
+	 *
+	 * @param string $data
+	 */
+	private function writeRequestLog($data)
+	{
+		if ($this->isDelayMode) {
+			$this->redis->rpush($this->redisList, $data);
 
-            if ($this->readyForWriting()) {
-                $this->pullCachedLogs();
-            }
-        } else {
-            $this->append($data);
-        }
-    }
+			if ($this->readyForWriting()) {
+				$this->pullCachedLogs();
+			}
+		} else {
+			$this->append($data);
+		}
+	}
 
-    /**
-     * Read history log from cache and then delete from cache.
-     *
-     * @return string
-     */
-    private function pullCachedLogs()
-    {
-        $len = $this->redis->llen($this->redisList);
-        $this->redis->multi();
-        $this->redis->lrange($this->redisList, 0, $len - 1);
-        $this->redis->ltrim($this->redisList, $len, -1);
-        $this->redis->set($this->redisTime, time());
-        $logs = $this->redis->exec();
+	/**
+	 * Read history log from cache and then delete from cache.
+	 *
+	 * @return string
+	 */
+	private function pullCachedLogs()
+	{
+		$len = $this->redis->llen($this->redisList);
+		$this->redis->multi();
+		$this->redis->lrange($this->redisList, 0, $len - 1);
+		$this->redis->ltrim($this->redisList, $len, -1);
+		$this->redis->set($this->redisTime, time());
+		$logs = $this->redis->exec();
 
-        $this->append($logs[0] ?? []);
-    }
+		$this->append($logs[0] ?? []);
+	}
 
-    /**
-     * Determine if logs is ready for writing in delay mode.
-     *
-     * @return bool
-     */
-    private function readyForWriting()
-    {
-        return app('redis')->llen($this->redisList) >= config('runningtime.delay.log')
-            || time() - app('redis')->get($this->redisTime) >= config('runningtime.delay.time');
-    }
+	/**
+	 * Determine if logs is ready for writing in delay mode.
+	 *
+	 * @return bool
+	 */
+	private function readyForWriting()
+	{
+		return app('redis')->llen($this->redisList) >= config('runningtime.delay.log')
+			|| time() - app('redis')->get($this->redisTime) >= config('runningtime.delay.time');
+	}
 
-    /**
-     * Append log data to file.
-     *
-     * @param string $data
-     */
-    private function append($data)
-    {
-        $logFilePath = config('runningtime.path') . '/' . date('Y-m-d') . '.log';
-        $this->checkLogDir();
+	/**
+	 * Append log data to file.
+	 *
+	 * @param string $data
+	 */
+	private function append($data)
+	{
+		$logFilePath = config('runningtime.path') . '/' . date('Y-m-d') . '.log';
+		$this->checkLogDir();
 
-        file_put_contents($logFilePath, $data, FILE_APPEND);
+		file_put_contents($logFilePath, $data, FILE_APPEND);
 
-        if ($this->isDelayMode) {
+		if ($this->isDelayMode) {
 
-        }
-    }
+		}
+	}
 
-    /**
-     * Ensure the path of logs exists.
-     *
-     * @return void
-     */
-    private function checkLogDir()
-    {
-        $path = config('runningtime.path', storage_path('logs/runningtime/'));
-        if (!is_dir($path)) {
-            mkdir($path, 0777, true);
-        }
-    }
+	/**
+	 * Ensure the path of logs exists.
+	 *
+	 * @return void
+	 */
+	private function checkLogDir()
+	{
+		$path = config('runningtime.path', storage_path('logs/runningtime/'));
+		if (!is_dir($path)) {
+			mkdir($path, 0777, true);
+		}
+	}
 }
